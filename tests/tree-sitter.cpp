b62ff556f0af6ccc9e9cd18d904c8e1667ef839c
Matthias Seiffert <matthias-seiffert@hotmail.de>
2020-09-05 12:09:04 +0200

    Remove plain tree-sitter tests

--- tests/tree-sitter.cpp
+++ tests/tree-sitter.cpp
@@ -1,6 +1,5 @@
 #include <catch2/catch.hpp>
 #include <cstring>
-#include <tree_sitter/api.h>
 #include <type_traits>
 
 #include "tree-sitter/tree-sitter.hpp"
@@ -655,33 +654,4 @@ end
             }
         }
     }
-}
-
-TEST_CASE("Tree-Sitter", "[tree-sitter][parser]") {
-    TSParser* parser = ts_parser_new();
-    ts_parser_set_language(parser, tree_sitter_lua());
-
-    const char* source_code = "1 + 2";
-    TSTree* tree = ts_parser_parse_string(parser, nullptr, source_code, strlen(source_code));
-
-    TSNode root_node = ts_tree_root_node(tree);
-    TSNode expr_node = ts_node_named_child(root_node, 0);
-    TSNode bin_op_node = ts_node_named_child(expr_node, 0);
-    TSNode number_1_node = ts_node_named_child(bin_op_node, 0);
-    TSNode number_2_node = ts_node_named_child(bin_op_node, 1);
-
-    CHECK(ts_node_type(root_node) == std::string{"program"});
-    CHECK(ts_node_type(expr_node) == std::string{"expression"});
-    CHECK(ts_node_type(bin_op_node) == std::string{"binary_operation"});
-    CHECK(ts_node_named_child_count(bin_op_node) == 2);
-    CHECK(ts_node_type(number_1_node) == std::string{"number"});
-    CHECK(ts_node_type(number_2_node) == std::string{"number"});
-
-    char* s = ts_node_string(root_node);
-
-    INFO(s);
-
-    free(s);
-    ts_tree_delete(tree);
-    ts_parser_delete(parser);
 }
