ec19149ce19809691dbfeb364777c1800a84316b
Tim Nolle <tn4799@gmail.com>
2020-09-07 17:01:43 +0200

    undo formating on enum type

--- include/MiniLua/luatoken.hpp
+++ include/MiniLua/luatoken.hpp
@@ -7,64 +7,22 @@
 using namespace std;
 
 struct LuaToken {
+    // clang-format off
     enum Type {
         NONE,
-        ADD,
-        SUB,
-        MUL,
-        DIV,
-        MOD,
-        POW,
-        LEN,
-        STRIP,
-        EVAL, //+, -, *, /, %, ^, #, $, "\"
-        EQ,
-        NEQ,
-        LEQ,
-        GEQ,
-        LT,
-        GT,
-        ASSIGN, //==, ~=, <=, >=, <, >, =
-        LCB,
-        RCB,
-        LRB,
-        RRB,
-        LSB,
-        RSB, //{, }, (, ), [, ]
-        SEM,
-        COLON,
-        COMMA,
-        DOT,
-        CONCAT,
-        ELLIPSE, //;, :, ,, ., .., ...
+        ADD, SUB, MUL, DIV, MOD, POW, LEN, STRIP, EVAL, //+, -, *, /, %, ^, #, $, "\"
+        EQ, NEQ, LEQ, GEQ, LT, GT, ASSIGN, //==, ~=, <=, >=, <, >, =
+        LCB, RCB, LRB, RRB, LSB, RSB, //{, }, (, ), [, ]
+        SEM, COLON, COMMA, DOT, CONCAT, ELLIPSE, //;, :, ,, ., .., ...
 
-        AND,
-        BREAK,
-        DO,
-        ELSE,
-        ELSEIF,
-        END,
-        FALSE,
-        FOR,
-        FUNCTION,
-        IF,
-        IN,
-        LOCAL,
-        NIL,
-        NOT,
-        OR,
-        REPEAT,
-        RETURN,
-        THEN,
-        TRUE,
-        UNTIL,
-        WHILE,
+        AND, BREAK, DO, ELSE, ELSEIF,
+        END, FALSE, FOR, FUNCTION, IF,
+        IN, LOCAL, NIL, NOT, OR,
+        REPEAT, RETURN, THEN, TRUE, UNTIL, WHILE,
 
-        NAME,
-        STRINGLIT,
-        NUMLIT,
-        COMMENT
+        NAME, STRINGLIT, NUMLIT, COMMENT
     } type;
+    //clang-format on
     string match;
     long pos = string::npos;
     long length = 0;
