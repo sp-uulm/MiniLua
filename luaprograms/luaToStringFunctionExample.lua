3be43915b032aea44521cf686fb768f95627d84b
Tim Nolle <tn4799@gmail.com>
2020-11-26 14:25:51 +0100

    added a luafile for tesing tostring of lua
    the program prints for every datatype in lua the result of the
    tostring-implementation of lua

---
+++ luaprograms/luaToStringFunctionExample.lua
@@ -0,0 +1,27 @@
+print("string")
+text = "25isjjd2"
+print(tostring(text))
+print("bool")
+bool = true
+print(tostring(bool))
+print("numbers")
+number = 2872
+float = 26.82736
+print(tostring(number))
+print(tostring(float))
+print("table")
+table = {}
+table.klein = "klein"
+table.zahl = 287632
+table.wahrheit = false
+print(tostring(table))
+
+print("function")
+function just4fun() 
+	print("im done")
+end
+print(tostring(just4fun))
+
+print("nil")
+nilValue = nil
+print(tostring(nilValue))
