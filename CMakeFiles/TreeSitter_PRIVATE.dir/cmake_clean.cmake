b7f86476d0f5bd2f15d9f8f9c8207bee242649ea
Tim Nolle <tn4799@gmail.com>
2020-12-03 17:23:05 +0100

    cleaned repo from unnessary cmakeshit

--- CMakeFiles/TreeSitter_PRIVATE.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/TreeSitter_PRIVATE"
-  "libtree-sitter.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/TreeSitter_PRIVATE.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/CMakeFiles/Tree-Sitter-Static-Lib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Tree-Sitter-Static-Lib"
-  "libtree-sitter.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Tree-Sitter-Static-Lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
