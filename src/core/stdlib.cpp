ed5fa56a3ac6411a5f093c0cee5750b0acf0d9e4
Tim Nolle <tn4799@gmail.com>
2020-12-03 14:50:53 +0100

    completed patterns for matching numbers

--- src/core/stdlib.cpp
+++ src/core/stdlib.cpp
@@ -1,4 +1,5 @@
 #include <sstream>
+#include <regex>
 
 #include "Minilua/values.hpp"
 
@@ -10,7 +11,8 @@ auto to_string(const CallContext& ctx) -> Value {
         [](Number n) { return n.to_literal(); },
         [](String s) { return s.value; },
         [](Table t) { //TODO: maybe improve the way to get the address.
-        //at the moment it could be that every time you call it the address has changed because of the cahange in the stack
+        //at the moment it could be that every time you call it the 
+        //address has changed because of the change in the stack
             ostringstream get_address;
             get_address << &t;
             return get_address.str(); },
@@ -26,16 +28,24 @@ auto to_string(const CallContext& ctx) -> Value {
 auto to_number(const CallContext& ctx) -> Value {
     auto number = ctx.arguments().get(0);
     auto base = ctx.arguments().get(1);
-    std::string pattern_number = "\\w*\\d+\\.?\\d";
-    std::string pattern_hex = "0x[\\dA-Fa-f]+\\.?[\\dA-Fa-f]";
-    std::string pattern_exp = "\\w*\\d+\\.?\\de-?\\d+";
+    
     if(base == Nil){
         //matches pattern \w*\d+\.?\d*
         //Yes: parse number to double
         //No: return Nil
-        std::string pattern_number = "\\w*\\d+\\.?\\d";
-        std::string pattern_hex = "0x[\\dA-Fa-f]+\\.?[\\dA-Fa-f]";
-        std::string pattern_exp = "\\w*\\d+\\.?\\de-?\\d+";
+        std::regex pattern_number = "\\s*\\d+\\.?\\d*";
+        std::regex pattern_hex = "\\s*0x[\\dA-Fa-f]+\\.?[\\dA-Fa-f]*";
+        std::regex pattern_exp = "\\s*\\d+\\.?\\d*[eE]-?\\d+";
+
+        if(std::regex_match(number, pattern_number)){
+
+        }else if (std::regex_match(number, pattern_hex)){
+
+        }else if (std::regex_match(number, pattern_exp)) {
+
+        }else{
+            //TODO: Not a valid number
+        }
     }else{
         //match again with pattern, but this time with 1 .
         //no .:
@@ -50,7 +60,8 @@ auto to_number(const CallContext& ctx) -> Value {
         [](Number n) { return n.to_literal(); },
         [](String s) { return s.value; },
         [](Table t) { //TODO: maybe improve the way to get the address.
-        //at the moment it could be that every time you call it the address has changed because of the cahange in the stack
+        //at the moment it could be that every time you call it the address 
+        //has changed because of the cahange in the stack
             ostringstream get_address;
             get_address << &t;
             return get_address.str(); },
