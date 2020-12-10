0b8e98dc6c9339660a097b58c799ff5a79feda7d
Tim Nolle <tn4799@gmail.com>
2020-12-10 13:11:38 +0100

    added hpp file for stdlib

---
+++ include/MiniLua/stdlib.hpp
@@ -0,0 +1,17 @@
+#ifndef STDLIB_HPP
+#define STDLIB_HPP
+
+#include <string>
+#include <utility>
+
+#include "MiniLua/values.hpp"
+
+namespace minilua{
+static auto split_string(std::string s, char c) -> std::pair<std::string, std::string>;
+
+auto to_string(const CallContext& ctx) -> Value;
+
+auto to_number(const CallContext& ctx) -> Value;
+}
+
+#endif
