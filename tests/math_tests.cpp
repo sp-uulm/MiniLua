594945da1c3c5af789c29ac34cb32145c9aff2d0
Tim Nolle <tn4799@gmail.com>
2021-01-13 15:37:46 +0100

    added tests for acos(x)

--- tests/math_tests.cpp
+++ tests/math_tests.cpp
@@ -78,3 +78,70 @@ TEST_CASE("math.abs(x)") {
         }
     }
 }
+
+TEST_CASE("math.acos(x)") {
+    minilua::Environment env;
+    minilua::CallContext ctx(&env);
+
+    SECTION("Integer") {
+        SECTION("x is not in [-1, 1]") {
+            int x = -2;
+            minilua::Vallist list = minilua::Vallist({minilua::Value(x)});
+            ctx = ctx.make_new(list);
+            CHECK(minilua::math::acos(ctx) == minilua::Value("nan"));
+
+            x = 2;
+            list = minilua::Vallist({minilua::Value(x)});
+            ctx = ctx.make_new(list);
+            CHECK(minilua::math::acos(ctx) == minilua::Value("nan"));
+        }
+
+        SECTION("x is in [-1, 1]") {
+            double x = -0.5;
+            minilua::Vallist list = minilua::Vallist({minilua::Value(x)});
+            ctx = ctx.make_new(list);
+            minilua::Number n = std::get<minilua::Number>(minilua::math::acos(ctx));
+            CHECK(n.value == Approx(2.0944));
+
+            x = 1;
+            list = minilua::Vallist({minilua::Value(x)});
+            ctx = ctx.make_new(list);
+            CHECK(minilua::math::acos(ctx) == minilua::Value(0));
+        }
+    }
+
+    SECTION("String") {
+        SECTION("x is not in [-1, 1]") {
+            std::string x = "-2";
+            minilua::Vallist list = minilua::Vallist({minilua::Value(x)});
+            ctx = ctx.make_new(list);
+            CHECK(minilua::math::acos(ctx) == minilua::Value("nan"));
+
+            x = "2";
+            list = minilua::Vallist({minilua::Value(x)});
+            ctx = ctx.make_new(list);
+            CHECK(minilua::math::acos(ctx) == minilua::Value("nan"));
+        }
+
+        SECTION("x is in [-1, 1]") {
+            std::string x = "-0.5";
+            minilua::Vallist list = minilua::Vallist({minilua::Value(x)});
+            ctx = ctx.make_new(list);
+            minilua::Number n = std::get<minilua::Number>(minilua::math::acos(ctx));
+            CHECK(n.value == Approx(2.0944));
+
+            x = "1";
+            list = minilua::Vallist({minilua::Value(x)});
+            ctx = ctx.make_new(list);
+            CHECK(minilua::math::acos(ctx) == minilua::Value(0));
+        }
+    }
+
+    SECTION("invalid input") {
+        std::string s = "Minilua";
+        minilua::Vallist list = minilua::Vallist({minilua::Value(s)});
+        ctx = ctx.make_new(list);
+        CHECK_THROWS_WITH(
+            minilua::math::acos(ctx), "bad argument #1 to abs (number expected, got string)");
+    }
+}
