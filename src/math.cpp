a6049405eff73db5fc93890b8fce0c1e0a44ad2f
Tim Nolle <tn4799@gmail.com>
2021-01-13 19:59:06 +0100

    implemented atan

--- src/math.cpp
+++ src/math.cpp
@@ -1,6 +1,9 @@
 #include <cmath>
 #include <iostream>
 #include <stdexcept>
+#include <string>
+#include <utility>
+#include <variant>
 
 #include "MiniLua/math.hpp"
 #include "MiniLua/stdlib.hpp"
@@ -11,8 +14,8 @@ namespace minilua::math {
 
 // All math-functions can be called be a number or a string that is formated like a number.
 // This information is not documented in the documentation. I just found that when I was testing the
-// erro behaviour for wrong input of the function in lua That is the reason why everywhere to_number
-// is called.
+// error behaviour for wrong input of the function in lua
+// That is the reason why everywhere to_number is called.
 
 auto abs(const CallContext& ctx) -> Value {
     auto x = ctx.arguments().get(0);
@@ -23,7 +26,8 @@ auto abs(const CallContext& ctx) -> Value {
 
         return Value(std::abs(num.value));
     } else {
-        throw std::runtime_error("bad argument #1 to abs (number expected, got " + x.type() + ")");
+        throw std::runtime_error(
+            "bad argument #1 to 'abs' (number expected, got " + x.type() + ")");
     }
 }
 
@@ -40,7 +44,8 @@ auto acos(const CallContext& ctx) -> Value {
             return Value("nan");
         }
     } else {
-        throw std::runtime_error("bad argument #1 to acos (number expected, got " + x.type() + ")");
+        throw std::runtime_error(
+            "bad argument #1 to 'acos' (number expected, got " + x.type() + ")");
     }
 }
 
@@ -57,12 +62,108 @@ auto asin(const CallContext& ctx) -> Value {
             return Value("nan");
         }
     } else {
-        throw std::runtime_error("bad argument #1 to asin (number expected, got " + x.type() + ")");
+        throw std::runtime_error(
+            "bad argument #1 to 'asin' (number expected, got " + x.type() + ")");
     }
 }
 
 auto atan(const CallContext& ctx) -> Value {
-    // unimplemented
+    auto x = ctx.arguments().get(0);
+    auto y = ctx.arguments().get(1);
+
+    return std::visit(
+        overloaded{
+            [](Number x, Number y) { return Value(std::atan2(x.value, y.value)); },
+            [ctx](Number x, String y) {
+                Vallist list = Vallist({Value(std::move(y))});
+                CallContext new_ctx = ctx.make_new(list);
+                auto res = to_number(new_ctx);
+
+                if (res != Nil()) {
+                    auto num = get<Number>(res);
+
+                    return Value(std::atan2(x.value, num.value));
+                } else {
+                    throw std::runtime_error(
+                        "bad argument #2 to 'atan' (number expected, got string)");
+                }
+            },
+            [](Number x, Nil /*unused*/) { return Value(std::atan2(x.value, 1)); },
+            [](Number /*unused*/, auto y) -> Value {
+                throw std::runtime_error(
+                    "bad argument #2 to 'atan' (number expected, got " + std::string(y.TYPE) + ")");
+            },
+            [ctx](String x, Number y) {
+                Vallist list = Vallist({Value(std::move(x))});
+                CallContext new_ctx = ctx.make_new(list);
+                auto res = to_number(new_ctx);
+
+                if (res != Nil()) {
+                    auto num = get<Number>(res);
+
+                    return Value(std::atan2(num.value, y.value));
+                } else {
+                    throw std::runtime_error(
+                        "bad argument #1 to 'atan' (number expected, got string)");
+                }
+            },
+            [ctx](String x, String y) {
+                Vallist list = Vallist({Value(std::move(x))});
+                CallContext new_ctx = ctx.make_new(list);
+                auto res1 = to_number(new_ctx);
+
+                if (res1 != Nil()) {
+                    list = Vallist({Value(std::move(y))});
+                    new_ctx = ctx.make_new(list);
+                    auto res2 = to_number(new_ctx);
+
+                    if (res2 != Nil()) {
+                        auto x1 = std::get<Number>(res1);
+                        auto x2 = std::get<Number>(res2);
+
+                        return Value(std::atan2(x1.value, x2.value));
+                    } else {
+                        throw std::runtime_error(
+                            "bad argument #2 to 'atan' (number expected, got string)");
+                    }
+                } else {
+                    throw std::runtime_error(
+                        "bad argument #1 to 'atan' (number expected, got string)");
+                }
+            },
+            [ctx](String x, Nil /*unused*/) {
+                Vallist list = Vallist({Value(std::move(x))});
+                CallContext new_ctx = ctx.make_new(list);
+                auto res = to_number(new_ctx);
+
+                if (res != Nil()) {
+                    auto num = get<Number>(res);
+
+                    return Value(std::atan2(num.value, 1));
+                } else {
+                    throw std::runtime_error(
+                        "bad argument #1 to 'atan' (number expected, got string)");
+                }
+            },
+            [ctx](String x, auto y) -> Value {
+                Vallist list = Vallist({Value(std::move(x))});
+                CallContext new_ctx = ctx.make_new(list);
+                auto res = to_number(new_ctx);
+
+                if (res != Nil()) {
+                    throw std::runtime_error(
+                        "bad argument #2 to 'atan' (number expected, got " + std::string(y.TYPE) +
+                        ")");
+                } else {
+                    throw std::runtime_error(
+                        "bad argument #1 to 'atan' (number expected, got string)");
+                }
+            },
+            [](auto x, auto /*unused*/) -> Value {
+                throw std::runtime_error(
+                    "bad argument #1 to 'atan' (number expected, got " + std::string(x.TYPE) + ")");
+            }},
+        x.raw(), y.raw());
 }
 
 auto ceil(const CallContext& ctx) -> Value {
@@ -74,7 +175,8 @@ auto ceil(const CallContext& ctx) -> Value {
 
         return Value(std::ceil(num.value));
     } else {
-        throw std::runtime_error("bad argument #1 to ceil (number expected, got " + x.type() + ")");
+        throw std::runtime_error(
+            "bad argument #1 to 'ceil' (number expected, got " + x.type() + ")");
     }
 }
 
@@ -87,7 +189,8 @@ auto cos(const CallContext& ctx) -> Value {
 
         return Value(std::cos(num.value));
     } else {
-        throw std::runtime_error("bad argument #1 to cos (number expected, got " + x.type() + ")");
+        throw std::runtime_error(
+            "bad argument #1 to 'cos' (number expected, got " + x.type() + ")");
     }
 }
 
@@ -101,7 +204,8 @@ auto deg(const CallContext& ctx) -> Value {
 
         return Value(deg);
     } else {
-        throw std::runtime_error("bad argument #1 to deg (number expected, got " + x.type() + ")");
+        throw std::runtime_error(
+            "bad argument #1 to 'deg' (number expected, got " + x.type() + ")");
     }
 }
 
@@ -114,7 +218,8 @@ auto exp(const CallContext& ctx) -> Value {
 
         return Value(std::exp(num.value));
     } else {
-        throw std::runtime_error("bad argument #1 to exp (number expected, got " + x.type() + ")");
+        throw std::runtime_error(
+            "bad argument #1 to 'exp' (number expected, got " + x.type() + ")");
     }
 }
 } // namespace minilua::math
