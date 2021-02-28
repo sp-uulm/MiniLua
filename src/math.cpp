ae10d7de70b5c92182417313f118d7f1731436b9
Matthias Seiffert <matthias-seiffert@hotmail.de>
2021-02-28 19:25:59 +0100

    Update src/math.cpp
    Co-authored-by: tn4799 <52242395+tn4799@users.noreply.github.com>

--- src/math.cpp
+++ src/math.cpp
@@ -320,7 +320,7 @@ auto ceil(const CallContext& ctx) -> Value {
                 return std::nullopt;
             }
             Number n = std::get<Number>(new_value);
-            if (std::fmod(n.as_float(), 1.0) == 0.0) {
+            if (!n.is_float()) {
                 return old_value.force(
                     new_value); // Just guessing that this is the right value because every
                                 // information about post-comma numbers is lost
