1f8c4ba320eaebdc84b96a418f42ffeb17eae4a0
Matthias Seiffert <matthias-seiffert@hotmail.de>
2021-03-29 11:58:14 +0200

    Implement setvbuf and add some documentation

--- luaprograms/unit_tests/io/file.lua
+++ luaprograms/unit_tests/io/file.lua
@@ -21,3 +21,22 @@ content = file.read(file, "a")
 -- close using io.close instead of file:close
 io.close(file)
 assert(content == "")
+
+-- setvbuf can't be tested except that the file still works after calling it
+file = io.open("/tmp/luatest.txt", "r")
+assert(file.setvbuf(file, "no") == true)
+content = file.read(file, "a")
+file.close(file)
+assert(content == "Hello, World!")
+
+file = io.open("/tmp/luatest.txt", "r")
+assert(file.setvbuf(file, "line") == true)
+content = file.read(file, "a")
+file.close(file)
+assert(content == "Hello, World!")
+
+file = io.open("/tmp/luatest.txt", "r")
+assert(file.setvbuf(file, "full", 8) == true)
+content = file.read(file, "a")
+file.close(file)
+assert(content == "Hello, World!")
