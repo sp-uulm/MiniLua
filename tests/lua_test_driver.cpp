74c6c19e674910200739acde9f0a94d1d8fe1f34
Matthias Seiffert <matthias-seiffert@hotmail.de>
2021-04-10 18:16:35 +0200

    Add testing of stdin/stdout/stderr to lua_test_driver

--- tests/lua_test_driver.cpp
+++ tests/lua_test_driver.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <iostream>
 #include <regex>
+#include <string>
 
 auto operator<<(std::ostream& o, const std::vector<minilua::SourceChange>& self) -> std::ostream& {
     o << "[";
@@ -19,11 +20,19 @@ auto operator<<(std::ostream& o, const std::vector<minilua::SourceChange>& self)
     return o << " ]";
 }
 
+static auto read_optional_file(const std::string& path) -> std::optional<std::string> {
+    try {
+        std::ifstream ifs;
+        ifs.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        ifs.open(path);
+        return std::string(std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>());
+    } catch (const std::exception&) {
+        return std::nullopt;
+    }
+}
+
 auto read_input_from_file(const std::string& path) -> std::string {
-    std::ifstream ifs;
-    ifs.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    ifs.open(path);
-    return std::string(std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>());
+    return read_optional_file(path).value();
 }
 
 // struct ExpectedChange
@@ -78,8 +87,37 @@ auto get_tests() -> std::vector<std::unique_ptr<BaseTest>>& { return tests; }
 
 void register_test(BaseTest* test) { tests.push_back(std::unique_ptr<BaseTest>(test)); }
 
+static auto change_extension(const std::string& path, const std::string& new_ext) -> std::string {
+    auto last_dot = path.find_last_of('.');
+    if (last_dot == std::string::npos) {
+        throw std::runtime_error("Path has no dot: " + path);
+    }
+
+    return path.substr(0, last_dot + 1) + new_ext;
+}
+
 void test_file(const std::string& file) {
     std::string program = read_input_from_file(file);
+
+    std::string stdin_path = change_extension(file, "in");
+    std::string stdout_path = change_extension(file, "out");
+    std::string stderr_path = change_extension(file, "err");
+
+    std::optional<std::string> stdin_str = read_optional_file(stdin_path);
+    std::optional<std::string> stdout_str = read_optional_file(stdout_path);
+    std::optional<std::string> stderr_str = read_optional_file(stderr_path);
+
+    // NOTE: We need to define streams here so they live long enough,
+    // even if we don't always set them later
+
+    // in stream to read from stdin (if any)
+    std::istringstream stdin_stream;
+    if (stdin_str.has_value()) {
+        stdin_stream = std::istringstream(stdin_str.value());
+    }
+    // empty out/err streams to capture out/err
+    std::ostringstream stdout_stream;
+    std::ostringstream stderr_stream;
 
     auto expect_strings = find_expect_strings(program);
 
@@ -98,11 +136,41 @@ void test_file(const std::string& file) {
     CAPTURE(parse_result.errors);
     REQUIRE(parse_result);
 
-    // evaluate
-    auto result = interpreter.evaluate();
+    // setup stdin/out/err
+    // if they are not provided we use the default
+    if (stdin_str.has_value()) {
+        interpreter.environment().set_stdin(&stdin_stream);
+    }
+    if (stdout_str.has_value()) {
+        interpreter.environment().set_stdout(&stdout_stream);
+    }
+    if (stderr_str.has_value()) {
+        interpreter.environment().set_stderr(&stderr_stream);
+    }
 
-    // check
-    for (auto& test : tests) {
-        test->run(result);
+    // evaluate
+    try {
+        auto result = interpreter.evaluate();
+
+        // check
+        for (auto& test : tests) {
+            test->run(result);
+        }
+    } catch (const minilua::InterpreterException& e) {
+        if (stderr_str.has_value()) {
+            e.print_stacktrace(stderr_stream);
+        }
+    }
+
+    // check stdout/err
+    if (stdout_str.has_value() && *stdout_str != stdout_stream.str()) {
+        INFO(*stdout_str);
+        INFO(stdout_stream.str());
+        FAIL("stdout did not match");
+    }
+    if (stderr_str.has_value() && *stderr_str != stderr_stream.str()) {
+        INFO(*stderr_str);
+        INFO(stderr_stream.str());
+        FAIL("stderr did not match");
     }
 }
