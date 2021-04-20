e55a698a6d05875aeb4252cb9b5b65a0a34080e2
Matthias Seiffert <matthias-seiffert@hotmail.de>
2021-04-20 21:35:59 +0200

    Add --quiet flag to bin

--- bin/main.cpp
+++ bin/main.cpp
@@ -6,21 +6,27 @@
 
 auto main(int argc, char* argv[]) -> int {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " [--trace] <program.lua>\n";
+        std::cerr << "Usage: " << argv[0] << " [--quiet][--trace] <program.lua>\n";
         return 1;
     }
 
     bool trace = false;
-    size_t file_index = 1;
-    if (argv[1] == "--trace"s) {
+    bool quiet = false;
+    size_t index = 1;
+
+    if (argv[index] == "--quiet"s) {
+        ++index;
+        quiet = true;
+    }
+    if (argv[index] == "--trace"s) {
+        ++index;
         trace = true;
-        file_index = 2;
     }
 
     minilua::Interpreter interpreter;
     interpreter.config().all(trace);
 
-    if (auto result = interpreter.parse_file(argv[file_index]); !result) {
+    if (auto result = interpreter.parse_file(argv[index]); !result) {
         std::cerr << "Failed to parse\nErrors:\n";
         for (const auto& error : result.errors) {
             std::cerr << " - " << error << "\n";
@@ -30,14 +36,16 @@ auto main(int argc, char* argv[]) -> int {
 
     try {
         auto result = interpreter.evaluate();
-        std::cerr << "Terminated successfullly with value:\n\t" << result.value.to_literal()
-                  << "\n";
-        if (result.source_change.has_value()) {
-            std::cerr << "and source changes:\n\t" << result.source_change.value() << "\n";
-        }
+        if (!quiet) {
+            std::cerr << "Terminated successfullly with value:\n\t" << result.value.to_literal()
+                      << "\n";
+            if (result.source_change.has_value()) {
+                std::cerr << "and source changes:\n\t" << result.source_change.value() << "\n";
+            }
 
-        // TODO pretty print
-        std::cerr << "\nThe value had origin: " << result.value.origin() << "\n";
+            // TODO pretty print
+            std::cerr << "\nThe value had origin: " << result.value.origin() << "\n";
+        }
     } catch (const minilua::InterpreterException& e) {
         // std::cerr << "Evaluation failed:\n";
         e.print_stacktrace(std::cerr);
