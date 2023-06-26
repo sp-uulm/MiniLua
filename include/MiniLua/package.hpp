bb95efb2f7cf87d357a245a95668fcbbd091c52f
Tim Nolle <tim.nolle@uni-ulm.de>
2023-06-26 17:03:52 +0200

    added new functions to hpp

--- include/MiniLua/package.hpp
+++ include/MiniLua/package.hpp
@@ -9,5 +9,7 @@ auto require(const CallContext& ctx) -> Value;
 
 namespace package {
 auto searchpath(const CallContext& ctx) -> Vallist;
+auto find_loader(const CallContext& ctx) -> Vallist;
+auto require(const CallContext &ctx) -> Value;
 } // end namespace package
 } // end namespace minilua
